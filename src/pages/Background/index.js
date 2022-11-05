05cba59fbc6c40908ad2187d140f9a370986972d
yqrashawn <namy.19@gmail.com>
2022-11-06 05:18:00 +0800

    feat: track mm events
    Signed-off-by: yqrashawn <namy.19@gmail.com>

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,5 +1,44 @@
+import browser from 'webextension-polyfill';
+import { stream, CloseMode, trace } from '@thi.ng/rstream';
+
 import '../../assets/img/icon-34.png';
 import '../../assets/img/icon-128.png';
 
-console.log('This is the background page.');
-console.log('Put the background scripts here.');
+const contentScriptStrem = stream({
+  id: 'from-content-script',
+  closeIn: CloseMode.NEVER,
+  closeOut: CloseMode.NEVER,
+  cache: true,
+});
+
+// debug
+// contentScriptStrem.subscribe(trace());
+
+function postMessage(port, msg) {
+  try {
+    port.postMessage();
+  } catch (err) {
+    if (err.message?.includes('object could not be cloned')) {
+      msg = JSON.parse(JSON.stringify(msg));
+      port.postMessage(msg);
+    } else {
+      console.error('[BACKGROUND postMessage]', err);
+    }
+  }
+}
+
+function onConnect(port) {
+  const post = (msg) => postMessage(port, msg);
+  if (port.name === 'content-script') {
+    port.onMessage.addListener((msg) =>
+      contentScriptStrem.next.call(contentScriptStrem, [msg, post])
+    );
+  }
+}
+
+function setup() {
+  browser.runtime.onConnect.addListener(onConnect);
+  return { contentScriptStrem };
+}
+
+setup();
--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,97 @@
-import { printLine } from './modules/print';
+import browser from 'webextension-polyfill';
+import { stream, CloseMode } from '@thi.ng/rstream';
+import { sideEffect, filter, map } from '@thi.ng/transducers';
+import { getSiteMetadata } from '@fluent-wallet/site-metadata';
 
-console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
+function error(...msg) {
+  console.log('[SUBSCRIBER ERROR] =>', ...msg);
+}
 
-printLine("Using the 'printLine' function from the Print Module");
+const s = stream({
+  id: 'message-listener',
+  closeIn: CloseMode.NEVER,
+  closeOut: CloseMode.NEVER,
+  cache: true,
+});
+
+function isSameOrigin(msg) {
+  return msg?.origin === location.origin;
+}
+
+function isToMetaMask(msg) {
+  return (
+    msg?.data?.target === 'metamask-contentscript' &&
+    msg.data?.data?.name === 'metamask-provider'
+  );
+}
+
+function isFromMetaMask(msg) {
+  return (
+    msg?.data?.target === 'metamask-inpage' &&
+    msg.data?.data?.name === 'metamask-provider'
+  );
+}
+
+function isRelatedToMetaMask(msg) {
+  return isFromMetaMask(msg) || isToMetaMask(msg);
+}
+
+function isRPCCall(msg) {
+  return msg.data.data?.data?.method;
+}
+
+function getMessageData(msg) {
+  const { data } = msg.data.data;
+  return {
+    origin: msg.origin,
+    data,
+    fromMetaMask: msg.data.target === 'metamask-contentscript',
+    toMetaMask: msg.data.target === 'metamask-inpage',
+  };
+}
+
+// prettier-ignore
+const transformedStream = s.transform(
+  filter(isSameOrigin),
+  filter(isRelatedToMetaMask)
+)
+ .transform(
+  map(getMessageData),
+  // debug
+  // sideEffect(console.log)
+);
+
+function registerSite(post) {
+  if (!s) return;
+  getSiteMetadata()
+    .then((metadata) => {
+      if (!metadata.icon) delete metadata.icon;
+      post({
+        data: {
+          method: 'wallet_registerSiteMetadata',
+          params: metadata,
+        },
+        __byMSP: true,
+        origin: location.host,
+      });
+    })
+    .catch(() => null);
+}
+
+function setup() {
+  const port = browser.runtime.connect({ name: 'content-script' });
+  registerSite(port.postMessage.bind(port));
+
+  window.addEventListener('message', s.next.bind(s));
+  transformedStream.subscribe({
+    next(data) {
+      data.__fromMSP = true;
+      port.postMessage.call(port, { ...data });
+    },
+    error(err) {
+      error(err);
+    },
+  });
+}
+
+setup();
