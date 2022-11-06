b0f2a760c7be6942f8fa1a6a172331a3821e93bc
yqrashawn <namy.19@gmail.com>
2022-11-06 13:01:00 +0800

    feat: add popup notification web ext utilities

---
+++ src/pages/Background/notification.js
@@ -0,0 +1,37 @@
+import browser from 'webextension-polyfill';
+import { show } from './popup.js';
+
+async function notificationOnClicked(notificationId) {
+  if (
+    notificationId.startsWith('https://') ||
+    notificationId.startsWith('chrome-extension://')
+  ) {
+    await show({ url: notificationId });
+  }
+}
+
+function subscribeToNotificationClicked() {
+  // none browser env
+  // if (typeof window === 'undefined') return;
+  // if (typeof global !== 'undefined') return;
+  if (!browser.notifications.onClicked.hasListener(notificationOnClicked)) {
+    browser.notifications.onClicked.addListener(notificationOnClicked);
+  }
+}
+
+export function create(id, options = {}) {
+  const {
+    type = 'basic',
+    message = 'Click to check DETAILED TX INFO',
+    title = 'SAFE 3 TX INFO',
+    iconUrl = 'icon-128.png',
+  } = options;
+  subscribeToNotificationClicked();
+  return browser.notifications.create(id ?? undefined, {
+    ...options,
+    type,
+    message,
+    title,
+    iconUrl,
+  });
+}
---
+++ src/pages/Background/popup.js
@@ -0,0 +1,164 @@
+import browser from 'webextension-polyfill';
+import { isFunction } from '@thi.ng/checks';
+
+const POPUP_HEIGHT = 600;
+const POPUP_WIDTH = 420;
+let FOCUS_LISTENER = null;
+let REMOVED_LISTENER = null;
+
+const focus = async (wid) => {
+  if (!FOCUS_LISTENER) {
+    FOCUS_LISTENER = () => focus(wid);
+    REMOVED_LISTENER = (removedWId) => {
+      if (removedWId === wid) {
+        browser.windows.onFocusChanged.hasListener(FOCUS_LISTENER) &&
+          browser.windows.onFocusChanged.removeListener(FOCUS_LISTENER);
+        FOCUS_LISTENER = null;
+        browser.windows.onRemoved.hasListener(REMOVED_LISTENER) &&
+          browser.windows.onRemoved.removeListener(REMOVED_LISTENER);
+        REMOVED_LISTENER = null;
+      }
+    };
+
+    browser.windows.onRemoved.addListener(REMOVED_LISTENER);
+    browser.windows.onFocusChanged.addListener(FOCUS_LISTENER);
+  } else {
+    try {
+      const w = await browser.windows.get(wid);
+      if (w) {
+        await browser.windows.update(wid, { focused: true });
+      }
+    } catch (err) {
+      // can't find window with id `wid`
+      if (FOCUS_LISTENER) {
+        browser.windows.onFocusChanged.hasListener(FOCUS_LISTENER) &&
+          browser.windows.onFocusChanged.removeListener(FOCUS_LISTENER);
+        FOCUS_LISTENER = null;
+      }
+    }
+  }
+};
+
+const newPopup = async ({ url, alwaysOnTop }) => {
+  const lastFocused = await browser.windows
+    .getLastFocused({ windowTypes: ['normal'] })
+    .catch(() => {});
+
+  const w = await browser.windows.create({
+    type: 'popup',
+    focused: true,
+    url,
+    width: POPUP_WIDTH,
+    height: POPUP_HEIGHT,
+    left: lastFocused?.left,
+    top: lastFocused?.top,
+  });
+
+  alwaysOnTop && focus(w.id);
+
+  return w;
+};
+
+export const show = async (arg = {}) => {
+  let { url, alwaysOnTop = false, mode = {} } = arg;
+  if (!browser?.windows?.getAll) return;
+  let popup = (await browser.windows.getAll()).filter(
+    (w) => w.type === 'popup'
+  )?.[0];
+  if (false && popup) {
+    try {
+      // for some reason, chrome won't throw the error if popup is destroyed
+      // so we add
+      // 1. setTimeout
+      // 2. browser.windows.get &&
+      // to ensure the logic goes into catch
+      await new Promise((resolve) => setTimeout(resolve, 1));
+      (await browser.windows.get(popup.id)) &&
+        (await browser.windows.update(popup.id, { focused: true }));
+      // eslint-disable-next-line no-empty
+    } catch (err) {
+      // popup got deleted
+      if (
+        popup.id !==
+        (await browser.windows.getAll()).filter((w) => w.type === 'popup')?.[0]
+          ?.id
+      ) {
+        show(arg);
+      }
+    }
+  } else {
+    popup = await newPopup({ url, alwaysOnTop });
+  }
+  return popup;
+};
+
+export const removePopup = async () => {
+  if (!browser?.windows?.getAll) return;
+  try {
+    const popup = (await browser.windows.getAll()).filter(
+      (w) => w.type === 'popup'
+    )?.[0];
+
+    if (popup) {
+      await remove(popup.id);
+    }
+    return true;
+  } catch (err) {} // eslint-disable-line no-empty
+  return false;
+};
+
+let ON_FOCUS_CHANGED = [];
+let ON_REMOVED = [];
+
+if (browser?.windows?.onFocusChanged) {
+  browser.windows.onFocusChanged.addListener((id) => {
+    ON_FOCUS_CHANGED = ON_FOCUS_CHANGED.reduce((acc, f) => {
+      try {
+        const dontRemoveListener = f(id);
+        if (dontRemoveListener) return acc.concat(f);
+      } catch (err) {} // eslint-disable-line no-empty
+
+      return acc;
+    }, []);
+  });
+
+  browser.windows.onRemoved.addListener((id) => {
+    ON_REMOVED = ON_REMOVED.reduce((acc, f) => {
+      try {
+        const dontRemoveListener = f(id);
+        if (dontRemoveListener === true) return acc.concat(f);
+      } catch (err) {} // eslint-disable-line no-empty
+
+      return acc;
+    }, []);
+  });
+}
+export const onFocusChanged = (windowId, f) => {
+  if (!isFunction(f)) throw new Error('Invalid callback, must be a function');
+  ON_FOCUS_CHANGED.push((id) => {
+    if (windowId === id) {
+      return f(id);
+    }
+    return true;
+  });
+};
+
+export const onRemoved = (windowId, f) => {
+  if (!isFunction(f)) throw new Error('Invalid callback, must be a function');
+  ON_REMOVED.push((id) => {
+    if (windowId === id) {
+      try {
+        return f(id);
+        // eslint-disable-next-line no-empty
+      } catch (err) {}
+    }
+    return true;
+  });
+};
+
+export const remove = (...args) => {
+  try {
+    return browser.windows?.remove(...args);
+    // eslint-disable-next-line no-empty
+  } catch (err) {}
+};
